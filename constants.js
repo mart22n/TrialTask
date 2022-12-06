7aadece07c00ed71cbadc7a2205fe2f454333ae7
mart22n <mart22n@gmail.com>
2022-12-06 23:56:55 +0200

    Update constants.js

--- constants.js
+++ constants.js
@@ -3,8 +3,10 @@ module.exports = Object.freeze({
 /* We use HttpMethod as an enum in case e.g. the literals
  "GET" or "POST" get changed in HTTP messages
  */
-    HttpMethod : {
+     HttpMethod : {
         GET: "GET",
         POST: "POST"
-    }
+    },
+    port : 3000,
+    hostName : "127.0.0.1"
 });
